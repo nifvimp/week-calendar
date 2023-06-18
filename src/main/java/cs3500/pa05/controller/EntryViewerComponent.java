2cee2c98fd18dcf98a47605d30a2082c5dd73286
nifvimp <kylewmoy@gmail.com>
2023-06-18 14:36:01 -0400

    Changes by walkerfryling

--- src/main/java/cs3500/pa05/controller/EntryViewerComponent.java
+++ src/main/java/cs3500/pa05/controller/EntryViewerComponent.java
@@ -5,20 +5,29 @@ import cs3500.pa05.model.Entry;
 import cs3500.pa05.model.Event;
 import cs3500.pa05.model.Task;
 import cs3500.pa05.model.TaskStatus;
+import cs3500.pa05.model.TimeInterval;
+import cs3500.pa05.model.Timestamp;
 import java.io.IOException;
+import java.util.Objects;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * A controller for an entry
  */
-public class EntryViewerComponent {
+public class EntryViewerComponent extends Dialog<Entry> {
   @FXML
   private TextField nameField;
   @FXML
@@ -34,10 +43,10 @@ public class EntryViewerComponent {
   @FXML
   private Button delete;
 
+  private BorderPane root;
+
   private Entry oldEntry;
-
   private JournalComponent journalComponent;
-
   private Rectangle background;
 
 
@@ -51,17 +60,33 @@ public class EntryViewerComponent {
     } catch (IOException exception) {
       throw new RuntimeException(exception);
     }
-    initElements(oldEntry, journalComponent);
+    this.oldEntry = oldEntry;
+    initElements(journalComponent);
+    this.initModality(Modality.NONE);
+    this.show();
   }
 
-  private void initElements(Entry oldEntry, JournalComponent journalComponent){
+  public EntryViewerComponent(JournalComponent journalComponent) {
+    this(null, journalComponent);
+  }
+
+  private void initElements(JournalComponent journalComponent){
     this.journalComponent = journalComponent;
-    this.oldEntry = oldEntry;
-    nameField.setText(oldEntry.name());
-    categoryField.setText(oldEntry.category()); // TODO: change to dropdown menu (comboBox)
-    descriptionField.setText(oldEntry.description());
-    if (oldEntry.isEvent()) {
-      entrySpecificInfo = new TextField(((Event) oldEntry).interval().toString());
+    String name = (oldEntry == null) ? "" : oldEntry.name();
+    String category = (oldEntry == null) ? "" : oldEntry.category();
+    String description = (oldEntry == null) ? "" : oldEntry.description();
+    nameField.setText(name);
+    categoryField.setText(category); // TODO: change to dropdown menu (comboBox)
+    descriptionField.setText(description);
+
+    if (oldEntry.isEvent()) { // This feels scuffed. Will probably redo when swapped out for  dropdown menu
+      if (oldEntry != null) {
+        entrySpecificInfo = new TextField(((Event) oldEntry).interval().toString());
+      } else {
+        Timestamp startTime = new Timestamp(DayOfWeek.TUESDAY, 0);
+        TimeInterval interval = new TimeInterval(startTime,15);
+        entrySpecificInfo = new TextField(interval.toString());
+      }
     } else if (oldEntry.isTask()) {
       entrySpecificInfo = new CheckBox();
       boolean status = ((Task) oldEntry).getStatus().equals(TaskStatus.COMPLETE);
